244703a41bef71e7d8d66f9781e81031555c3c05
zactech178 <fullstacksoftwaredev@gmail.com>
2021-04-06 10:01:46 -0700

    reset password

--- routes/routesAccount.js
+++ routes/routesAccount.js
@@ -138,4 +138,32 @@ router.post('/update-profile', function(req, res, next) {
   }
 });
 
+router.post('/reset-password', function(req, res, next) {
+
+  try {
+
+    users.updateOne({email: req.body.email, password: req.body.password}, {password: req.body.newPassword}, (err, data) => {
+      if (err) {
+        return res.status(500).send({ message: 'Internal server error'});
+      } else if (data != undefined && data != null) {
+        if (data.length == 0) {
+          return res.status(401).send({ message: 'Password mismatch'});
+        }
+        console.log(data);
+        users.findOne({email: req.body.email, password: req.body.newPassword}, (err, doc) => {
+          const accessToken = jwt.sign({ userId: doc._id }, JWT_SECRET, {
+            expiresIn: JWT_EXPIRES_IN
+          });
+         
+          return res.json({ status: 200, user: doc,  accessToken });
+        });
+      } else {
+        return res.status(401).send({ message: 'Password mismatch'});
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: 'Internal server error'});
+  }
+});
 module.exports = router;
