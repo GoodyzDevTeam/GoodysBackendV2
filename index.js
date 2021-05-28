841cffd278a252ce0a4a5e3dcc894f58665913df
ihor <ihor137@outlook.com>
2021-05-27 22:13:58 -0700

    minor fix

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require("helmet");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
 
 /* app.use # packages */
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(compression());
 app.use(cors());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
 
 /* Routes */
 const indexRoute = require('./routes/index');
