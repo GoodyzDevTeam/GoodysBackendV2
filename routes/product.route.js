f99959ddfadd0d759f73081cee7f77b2ce8c27e0
ihor <ihor137@outlook.com>
2021-06-10 10:20:17 -0700

    get featured, top product, basic routing

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,9 +5,13 @@ const { auth } = require('../auth/auth');
 const { getAllProductsController } = require('../controllers/product/get.all.product.controller');
 const { getLikeProductsController } = require('../controllers/product/get.like.product.controller');
 const { getAllCategoriesController } = require('../controllers/product/get.all.category.controller');
+const { getTopProductsController } = require('../controllers/product/get.top.product.controller');
+const { getFeaturedProductsController } = require('../controllers/product/get.featured.product.controller');
 
 router.get('/', auth, getAllProductsController);
 router.get('/category', auth, getAllCategoriesController);
 router.get('/like-products', auth, getLikeProductsController);
+router.get('/top-products', auth, getTopProductsController);
+router.get('/featured-products', auth, getFeaturedProductsController);
 
 module.exports = router;
